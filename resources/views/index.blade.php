432df6f98c9f8815079b55f3850b02379fd178d1
HTM <haythamaaraba7@gmail.com>
2022-10-15 20:45:19 +0100

    Navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--Css-->
+    <link rel="stylesheet" href="css/style.css">
+    
+    <!--fontawesome-->
+    <script
+      src="https://kit.fontawesome.com/84ffe9b9c0.js"
+      crossorigin="anonymous"
+    ></script>
+
     <title>Hotelz</title>
 </head>
 <body>
--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -1,3 +1,17 @@
 <header>
-    
+    <div class="logo">
+        <a href=""><p>Hotelz</p></a>
+    </div>
+    <form action="/search" method="GET" class="search-bar">
+        <input type="text" placeholder="Search.." name="query" />
+        <button type="submit"><i class="fa fa-search"></i></button>
+    </form>
+    <div class="navigation">
+        <ul>
+            <li><a href="">Hotels</a></li>
+            <li><a href="">Suggestions</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Social</a></li>
+        </ul>
+    </div>
 </header>
