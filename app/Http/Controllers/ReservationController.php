b63638ca7558fd326392580a17179dbcbac33931
HTM <haythamaaraba7@gmail.com>
2023-01-21 13:52:34 +0100

    creating the reservation

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Hotel;
 use App\Models\Room;
+use App\Models\Hotel;
+use App\Models\Reservation;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
@@ -11,14 +12,82 @@ class ReservationController extends Controller
     // create a reservation
     public function create(Request $request)
     {
-        // check rooms avaibility
-        $hotel = Hotel::find($request->hotel_id);
-        $StandardNumber = Room::where('hotel_id', $request->hotel_id)->where('type', 'standard')->get()->count();
+        $formFields = $request->validate([
+            'hotel_id' => 'required',
+            'people_number' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required'
+        ]);
 
-        $DeluxeNumber = Room::where('hotel_id', $request->hotel_id)->where('type', 'deluxe')->get()->count();
+        // adding standard rooms to the reservation
+        if (in_array("standard", $request->rooms)) {
+            // standard room that are not reserved
+            $standardAvailableRooms = Room::where('type', 'standard')
+                ->where('hotel_id', $formFields['hotel_id'])
+                ->whereNotIn('id', function ($query) use ($formFields) {
+                    $query->select('room_id')
+                        ->from('reservations')
+                        ->where('start_date', '<=', $formFields['start_date'])
+                        ->where('end_date', '>=', $formFields['end_date']);
+                })->get('id');
 
-        $VipNumber = Room::where('hotel_id', $request->hotel_id)->where('type', 'vip')->get()->count();
+            // creating the reservations
+            for ($i=0; $i < $request->standard_number; $i++) { 
+                Reservation::create([
+                    'room_id' => $standardAvailableRooms[$i]->id,
+                    'client_id' => auth()->user()->id,
+                    'start_date' => $formFields['start_date'],
+                    'end_date' => $formFields['end_date']
+                ]);
+            }
+        }
 
-        dd($StandardNumber);
+        // adding deluxe rooms to the reservation
+        if (in_array("deluxe", $request->rooms)) {
+            // deluxe room that are not reserved
+            $deluxeAvailableRooms = Room::where('type', 'deluxe')
+                ->where('hotel_id', $formFields['hotel_id'])
+                ->whereNotIn('id', function ($query) use ($formFields) {
+                    $query->select('room_id')
+                        ->from('reservations')
+                        ->where('start_date', '<=', $formFields['start_date'])
+                        ->where('end_date', '>=', $formFields['end_date']);
+                })->get('id');
+
+            // creating the reservations
+            for ($i=0; $i < $request->deluxe_number; $i++) { 
+                Reservation::create([
+                    'room_id' => $deluxeAvailableRooms[$i]->id,
+                    'client_id' => auth()->user()->id,
+                    'start_date' => $formFields['start_date'],
+                    'end_date' => $formFields['end_date']
+                ]);
+            }
+        }
+
+        // adding vip rooms to the reservation
+        if (in_array("vip", $request->rooms)) {
+            // vip room that are not reserved
+            $vipAvailableRooms = Room::where('type', 'vip')
+                ->where('hotel_id', $formFields['hotel_id'])
+                ->whereNotIn('id', function ($query) use ($formFields) {
+                    $query->select('room_id')
+                        ->from('reservations')
+                        ->where('start_date', '<=', $formFields['start_date'])
+                        ->where('end_date', '>=', $formFields['end_date']);
+                })->get('id');
+
+            // creating the reservations
+            for ($i=0; $i < $request->vip_number; $i++) { 
+                Reservation::create([
+                    'room_id' => $vipAvailableRooms[$i]->id,
+                    'client_id' => auth()->user()->id,
+                    'start_date' => $formFields['start_date'],
+                    'end_date' => $formFields['end_date']
+                ]);
+            }
+        }
+
+        return redirect()->route('home')->with('message', 'Reservation created successfully');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Models\User;
+use App\Models\Hotel;
+use App\Models\Reservation;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\HotelController;
-use App\Models\Hotel;
-use App\Models\Reservation;
-use App\Models\User;
+use App\Http\Controllers\ReservationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +21,7 @@ use App\Models\User;
 
 Route::get('/', function () {
     return view('index', ['hotels' => Hotel::latest()->filter(request(['search']))->paginate(6)]);
-});
+})->name('home');
 
 // Hotels
 Route::prefix('hotels')->controller(HotelController::class)->group(function() {
